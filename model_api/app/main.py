87ad23df0c06bfd1397b0007dfa981f7ad999622
John Cummings <jcummingsutk@gmail.com>
2024-06-21 09:51:10 -0400

    updated model pull, api

--- model_api/app/main.py
+++ model_api/app/main.py
@@ -1,11 +1,35 @@
+import json
+
 import mlflow
+import numpy as np
+import pandas as pd
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class MovieInput(BaseModel):
+    userId: list[int]
+    movieId: list[int]
+
 
 app = FastAPI()
 
 
-@app.get("/")
-async def evaluate():
+@app.post("/predict")
+async def evaluate(movie_input: MovieInput):
+    movie_input_dict = movie_input.model_dump()
+    df_input = pd.DataFrame(movie_input_dict)
+
     model = mlflow.pyfunc.load_model("./model")
-    print(type(model))
-    return {"message": "Hello World"}
+    preprocessor = mlflow.pyfunc.load_model("./preprocessor")
+    df_input: pd.DataFrame = preprocessor.predict(df_input)
+    predictions_np: np.ndarray = model.predict(df_input).detach().numpy()
+    reshaped = predictions_np.reshape(1, -1).tolist()[0]
+    return {"predictions": json.dumps(reshaped)}
+
+
+@app.post("/metrics")
+async def get_metrics():
+    with open("./metrics.json", "r") as fp:
+        metrics_dict = json.load(fp)
+    return metrics_dict
--- model_api/download_model_artifacts.py
+++ model_api/download_model_artifacts.py
@@ -1,22 +1,68 @@
 import argparse
+import json
 import os
 import shutil
+from argparse import Namespace
+from typing import Any
 
+import mlflow
 from azure.ai.ml import MLClient
 from azure.identity import EnvironmentCredential
 from config import get_config_dict, load_azure_service_principal_environment_vars
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--download-path", type=str)
-    args = parser.parse_args()
 
-    os.makedirs(args.download_path, exist_ok=True)
+def parse_args() -> Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model-download-path", type=str)
+    parser.add_argument("--preprocessor-download-path", type=str)
+    args = parser.parse_args()
+    return args
+
+
+def setup_download_dirs(model_download_path: str, preprocessor_download_path: str):
+    if os.path.isdir(model_download_path):
+        shutil.rmtree(model_download_path)
+    if os.path.isdir(preprocessor_download_path):
+        shutil.rmtree(preprocessor_download_path)
+    os.makedirs(model_download_path, exist_ok=True)
+    os.makedirs(preprocessor_download_path, exist_ok=True)
+
+
+def get_model_metrics(
+    ml_client: MLClient, model_name: str, model_version: str
+) -> dict[str, dict[str, Any]]:
+    model = ml_client.models.get(model_name, model_version)
+    job_name = model.job_name
+    mlflow_tracking_uri = ml_client.workspaces.get(
+        ml_client.workspace_name
+    ).mlflow_tracking_uri
+    mlflow.set_tracking_uri(mlflow_tracking_uri)
+    client = mlflow.tracking.MlflowClient()
+    metrics = ["train_mae", "test_mae", "train_rmse", "test_rmse", "r_precision_at_5"]
+    metrics_dict: dict[str, dict[str, Any]] = {}
+    for metric_name in metrics:
+        retrieved_metric = client.get_metric_history(job_name, metric_name)
+        retrieved_metric_as_dict = dict(retrieved_metric[0])
+        metrics_dict[retrieved_metric_as_dict["key"]] = {
+            key: val for key, val in retrieved_metric_as_dict.items() if key != "key"
+        }
+    return metrics_dict
+
+
+def main():
+
+    args = parse_args()
+    model_download_path = args.model_download_path
+    preprocessor_download_path = args.preprocessor_download_path
+    setup_download_dirs(model_download_path, preprocessor_download_path)
 
     load_azure_service_principal_environment_vars("config.yaml", "config_secret.yaml")
     config_dict = get_config_dict("config.yaml")
+
     model_name = config_dict["model"]["name"]
     model_version = config_dict["model"]["version"]
+    preprocessor_name = config_dict["preprocessor"]["name"]
+    preprocessor_version = config_dict["preprocessor"]["version"]
 
     cred = EnvironmentCredential()
 
@@ -26,10 +72,49 @@ if __name__ == "__main__":
         workspace_name="movie-recommender-dev",
         credential=cred,
     )
-
-    ml_client.models.download(model_name, model_version, args.download_path)
-
-    shutil.copytree(
-        os.path.join(args.download_path, "movie-recommender", "model"),
-        os.path.join("model_api", "model"),
+    ml_client.models.download(
+        name=model_name,
+        version=model_version,
+        download_path=model_download_path,
     )
+
+    ml_client.models.download(
+        name=preprocessor_name,
+        version=preprocessor_version,
+        download_path=preprocessor_download_path,
+    )
+
+    model_dir = os.path.join("model_api", "model")
+    preprocessor_dir = os.path.join("model_api", "preprocessor")
+
+    if os.path.isdir(model_dir):
+        shutil.rmtree(model_dir)
+    if os.path.isdir(preprocessor_dir):
+        shutil.rmtree(preprocessor_dir)
+
+    shutil.move(
+        os.path.join(model_download_path, model_name, "model"),
+        "model_api",
+    )
+    shutil.rmtree(model_download_path)
+
+    shutil.move(
+        os.path.join(preprocessor_download_path, preprocessor_name, "preprocessor"),
+        "model_api",
+    )
+    shutil.rmtree(preprocessor_download_path)
+
+    metrics_dict = get_model_metrics(ml_client, model_name, model_version)
+    print(metrics_dict)
+
+    metrics_file = os.path.join("model_api", "metrics.json")
+
+    if os.path.isfile(metrics_file):
+        os.remove(path=metrics_file)
+
+    with open(metrics_file, "w") as fp:
+        json.dump(metrics_dict, fp, indent=4)
+
+
+if __name__ == "__main__":
+    main()
